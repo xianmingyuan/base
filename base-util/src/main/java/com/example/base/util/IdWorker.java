cf18684db97cb1075ade15f89c4589248799bd39
xianmingyuan <xianmingyuan@tuandai.com>
2018-10-25 15:31:52 +0800

    refactor(location): 重写IdWorker类

--- base-util/src/main/java/com/example/base/util/IdWorker.java
+++ base-util/src/main/java/com/example/base/util/IdWorker.java
@@ -7,77 +7,144 @@ import lombok.extern.slf4j.Slf4j;
  * @author xianmingyuan
  */
 @Slf4j
-public class IdWorker{
+public class IdWorker {
 
+    /**
+     * 开始时间截
+     */
+    private final long startTime;
+
+    /**
+     * 机器id所占的位数
+     */
+    private static final long WORKER_ID_BITS = 5L;
+
+    /**
+     * 数据标识id所占的位数
+     */
+    private static final long DATA_CENTER_ID_BITS = 5L;
+
+    /**
+     * 支持的最大机器id，结果是31 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数)
+     */
+    private static final long MAX_WORKER_ID = ~(-1L << WORKER_ID_BITS);
+
+    /**
+     * 支持的最大数据标识id，结果是31
+     */
+    private static final long MAX_DATA_CENTER_ID = ~(-1L << DATA_CENTER_ID_BITS);
+
+    /**
+     * 序列在id中占的位数
+     */
+    private static final long SEQUENCE_BITS = 12L;
+
+    /**
+     * 机器ID向左移12位
+     */
+    private static final long WORKER_ID_SHIFT = SEQUENCE_BITS;
+
+    /**
+     * 数据标识id向左移17位(12+5)
+     */
+    private static final long DATA_CENTER_ID_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS;
+
+    /**
+     * 时间截向左移22位(5+5+12)
+     */
+    private static final long TIMESTAMP_LEFT_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS + DATA_CENTER_ID_BITS;
+
+    /**
+     * 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095)
+     */
+    private static final long SEQUENCE_MASK = ~(-1L << SEQUENCE_BITS);
+
+    /**
+     * 工作机器ID(0~31)
+     */
     private long workerId;
+
+    /**
+     * 数据中心ID(0~31)
+     */
     private long dataCenterId;
-    private long sequence;
 
-    public IdWorker(long workerId, long dataCenterId, long sequence){
-        if (workerId > maxWorkerId || workerId < 0) {
-            throw new IllegalArgumentException(String.format("worker Id can't be greater than %d or less than 0",maxWorkerId));
-        }
-        if (dataCenterId > maxDatacenterId || dataCenterId < 0) {
-            throw new IllegalArgumentException(String.format("dataCenter Id can't be greater than %d or less than 0",maxDatacenterId));
-        }
-        log.info("worker starting. timestamp left shift {}, dataCenter id bits {}, worker id bits {}, sequence bits {}, workerid {}", timestampLeftShift, datacenterIdBits, workerIdBits, sequenceBits, workerId);
-        this.workerId = workerId;
-        this.dataCenterId = dataCenterId;
-        this.sequence = sequence;
-    }
+    /**
+     * 毫秒内序列(0~4095)
+     */
+    private long sequence = 0L;
 
-    private long twepoch = 1288834974657L;
-
-    private long workerIdBits = 5L;
-    private long datacenterIdBits = 5L;
-    private long maxWorkerId = ~(-1L << workerIdBits);
-    private long maxDatacenterId = ~(-1L << datacenterIdBits);
-    private long sequenceBits = 12L;
-
-    private long workerIdShift = sequenceBits;
-    private long datacenterIdShift = sequenceBits + workerIdBits;
-    private long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
-    private long sequenceMask = ~(-1L << sequenceBits);
-
+    /**
+     * 上次生成ID的时间截
+     */
     private long lastTimestamp = -1L;
 
-    public long getWorkerId(){
-        return workerId;
+    //==============================Constructors=====================================
+
+    /**
+     * 构造函数
+     *
+     * @param workerId     工作ID (0~31)
+     * @param dataCenterId 数据中心ID (0~31)
+     */
+    public IdWorker(long startTime, long workerId, long dataCenterId) {
+        this.startTime = startTime;
+        if (workerId > MAX_WORKER_ID || workerId < 0) {
+            throw new IllegalArgumentException(String.format("worker Id can't be greater than %d or less than 0", MAX_WORKER_ID));
+        }
+        if (dataCenterId > MAX_DATA_CENTER_ID || dataCenterId < 0) {
+            throw new IllegalArgumentException(String.format("datacenter Id can't be greater than %d or less than 0", MAX_DATA_CENTER_ID));
+        }
+        this.workerId = workerId;
+        this.dataCenterId = dataCenterId;
     }
 
-    public long getDatacenterId(){
-        return dataCenterId;
-    }
+    // ==============================Methods==========================================
 
-    public long getTimestamp(){
-        return System.currentTimeMillis();
-    }
-
+    /**
+     * 获得下一个ID (该方法是线程安全的)
+     *
+     * @return SnowflakeId
+     */
     public synchronized long nextId() {
         long timestamp = timeGen();
 
+        //如果当前时间小于上一次ID生成的时间戳，说明系统时钟回退过这个时候应当抛出异常
         if (timestamp < lastTimestamp) {
-            log.error("clock is moving backwards.  Rejecting requests until {}.", lastTimestamp);
-            throw new IdWorkerException(String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds",
-                    lastTimestamp - timestamp));
+            throw new IdWorkerException(
+                    String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
         }
 
+        //如果是同一时间生成的，则进行毫秒内序列
         if (lastTimestamp == timestamp) {
-            sequence = (sequence + 1) & sequenceMask;
+            sequence = (sequence + 1) & SEQUENCE_MASK;
+            //毫秒内序列溢出
             if (sequence == 0) {
+                //阻塞到下一个毫秒,获得新的时间戳
                 timestamp = tilNextMillis(lastTimestamp);
             }
-        } else {
-            sequence = 0;
+        }
+        //时间戳改变，毫秒内序列重置
+        else {
+            sequence = 0L;
         }
 
+        //上次生成ID的时间截
         lastTimestamp = timestamp;
-        return ((timestamp - twepoch) << timestampLeftShift) |
-                (dataCenterId << datacenterIdShift) |
-                (workerId << workerIdShift) |
-                sequence;
+
+        //移位并通过或运算拼到一起组成64位的ID
+        return ((timestamp - startTime) << TIMESTAMP_LEFT_SHIFT)
+                | (dataCenterId << DATA_CENTER_ID_SHIFT)
+                | (workerId << WORKER_ID_SHIFT)
+                | sequence;
     }
 
+    /**
+     * 阻塞到下一个毫秒，直到获得新的时间戳
+     *
+     * @param lastTimestamp 上次生成ID的时间截
+     * @return 当前时间戳
+     */
     private long tilNextMillis(long lastTimestamp) {
         long timestamp = timeGen();
         while (timestamp <= lastTimestamp) {
@@ -86,15 +153,13 @@ public class IdWorker{
         return timestamp;
     }
 
-    private long timeGen(){
+    /**
+     * 返回以毫秒为单位的当前时间
+     *
+     * @return 当前时间(毫秒)
+     */
+    private long timeGen() {
         return System.currentTimeMillis();
-    }
-
-    public static void main(String[] args) {
-        IdWorker worker = new IdWorker(1,1,1);
-        for (int i = 0; i < 30; i++) {
-            System.out.println(worker.nextId());
-        }
     }
 
 }
