d21fb6659249406715fba322ae2c89d3f92838e5
xianmingyuan <1242440175@qq.com>
2018-10-28 22:58:31 +0800

    test(location): idWork

--- base-authentication/src/test/java/com/example/base/util/IdWorkerTest.java
+++ base-authentication/src/test/java/com/example/base/util/IdWorkerTest.java
@@ -3,18 +3,12 @@ package com.example.base.util;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 @Slf4j
 public class IdWorkerTest {
 
     @Test
-    public void nextId() throws ParseException {
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date date = format.parse("2018-10-25 00:00:00");
-        IdWorker idWorker = new IdWorker(date.getTime(), 0, 0);
+    public void nextId() {
+        IdWorker idWorker = new IdWorker(0, 0);
         // 防止黑客猜到ID顺序可以休眠
         for (int i = 0; i < 1000; i++) {
             long id = idWorker.nextId();
